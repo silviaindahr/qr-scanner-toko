90cb80a95926338f740733a6ff2677e755c1d26e
silviaindahr <119873179+silviaindahr@users.noreply.github.com>
2026-01-19 23:58:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,11 @@
 const CACHE_NAME = "qr-scanner-v1";
 
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/manifest.json"
-];
-
-// pas pertama kali install
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-// pas web dibuka
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
