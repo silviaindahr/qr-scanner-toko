f0458371cf9267793d5862c23fa9073cd13f9cb5
silviaindahr <119873179+silviaindahr@users.noreply.github.com>
2026-01-21 21:53:23 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,5 +3,18 @@ self.addEventListener("install", () => {
 });
 
 self.addEventListener("fetch", () => {
-  // kosong juga GAPAPA untuk awal
 });
+function startScan() {
+  const html5QrCode = new Html5Qrcode("reader");
+
+  html5QrCode.start(
+    { facingMode: "environment" },
+    { fps: 10, qrbox: 250 },
+    (decodedText) => {
+      document.getElementById("tokoId").value = decodedText;
+      html5QrCode.stop();
+      ambilData(); // langsung ambil data
+    }
+  );
+}
+;
