ad7b08a4d398c45fa5be692372b1991b56111f34
silviaindahr <119873179+silviaindahr@users.noreply.github.com>
2026-01-22 22:33:52 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,5 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+self.addEventListener('install',e=>{
+self.skipWaiting();
 });
 
-self.addEventListener("fetch", () => {
-});
-function startScan() {
-  const html5QrCode = new Html5Qrcode("reader");
-
-  html5QrCode.start(
-    { facingMode: "environment" },
-    { fps: 10, qrbox: 250 },
-    (decodedText) => {
-      document.getElementById("tokoId").value = decodedText;
-      html5QrCode.stop();
-      ambilData(); // langsung ambil data
-    }
-  );
-}
-;
+self.addEventListener('fetch',()=>{});
